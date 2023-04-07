6f93c9f00aecbd5b6642b8ba8998f8fc44c08764
chung-dajin <129668708+chung-dajin@users.noreply.github.com>
2023-04-07 17:13:09 +0900

    [Bronze V] Title: 시험 성적, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/9498. 시험 성적/시험 성적.py
@@ -0,0 +1,11 @@
+s =int(input())
+if s>89 and s<101:
+    print("A")
+elif s>79 and s<90:
+    print("B")
+elif s>69 and s<80:
+    print("C")
+elif s>59 and s<70:
+    print("D")
+else :
+    print("F")
