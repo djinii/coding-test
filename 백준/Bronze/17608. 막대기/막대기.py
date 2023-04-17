a75b20bb7c55b4c048b795c3839a265d1a24c9b6
djinii <129668708+djinii@users.noreply.github.com>
2023-04-17 14:47:36 +0900

    [Bronze II] Title: 막대기, Time: 132 ms, Memory: 35108 KB -BaekjoonHub

---
+++ 백준/Bronze/17608. 막대기/막대기.py
@@ -0,0 +1,17 @@
+import sys
+N=int(sys.stdin.readline())
+sticks=[]
+now=0
+cnt=1
+for i in range(N):
+    stick=int(sys.stdin.readline())
+    sticks.append(stick)
+    
+now=sticks[-1]
+for j in range(len(sticks)-1): 
+    sticks.pop()
+    if now<sticks[-1]:
+        cnt+=1
+        now=sticks[-1]
+        
+print(cnt)
