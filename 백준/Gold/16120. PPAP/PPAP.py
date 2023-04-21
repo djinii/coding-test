a85dcef85c87c6b8eb4965c764cbd08d6c1298ee
djinii <129668708+djinii@users.noreply.github.com>
2023-04-21 13:28:51 +0900

    [Gold IV] Title: PPAP, Time: 360 ms, Memory: 38308 KB -BaekjoonHub

--- 백준/Gold/16120. PPAP/PPAP.py
+++ 백준/Gold/16120. PPAP/PPAP.py
@@ -1,12 +1,13 @@
-str_ppap = input()
-stack = []
-ppap = ['P','P','A','P']
-for char_ppap in str_ppap:
-    stack.append(char_ppap)
+import sys
+p=sys.stdin.readline().strip()
+stack=[]
+ppap=['P','P','A','P']
+for i in p:
+    stack.append(i)
     if stack[-4:] == ppap:
         for _ in range(3):
             stack.pop()
-if stack == ppap or stack == ['P']:
-    print("PPAP")
+if stack == ppap or stack==['P']:
+    print('PPAP')
 else:
     print("NP")
