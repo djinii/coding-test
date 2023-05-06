1ec4fa1d58e2fba8ac0ba8a68fa98e66d7d30244
djinii <129668708+djinii@users.noreply.github.com>
2023-05-06 18:51:18 +0900

    [Silver III] Title: 최고의 피자, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/5545. 최고의 피자/최고의 피자.py
+++ 백준/Silver/5545. 최고의 피자/최고의 피자.py
@@ -2,19 +2,19 @@ n=int(input())
 dough_price, topping_price = map(int,input().split())
 dough_cal=int(input())
 caloreies=[]
+
 for _ in range(n):
     topping_cal=int(input())
     caloreies.append(topping_cal)
 
+total_cal=dough_cal 
 total_price=dough_price
-total_cal=dough_cal
 caloreies.sort(reverse=True)
 
 for i in range(n):
-    if total_cal/total_price < (total_cal+caloreies[i])/(total_price+topping_price):
+    if total_cal/total_price<(total_cal+caloreies[i])/(total_price+topping_price):
         total_cal+=caloreies[i]
         total_price+=topping_price
     else:
         break
-
 print(int(total_cal/total_price))
