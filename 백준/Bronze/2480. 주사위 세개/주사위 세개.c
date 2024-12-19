a0f968232a671f1bd6459b0b9f1dc3b9a2ff4f46
djinii <129668708+djinii@users.noreply.github.com>
2024-12-19 18:24:20 +0900

    [Bronze IV] Title: 주사위 세개, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Bronze/2480. 주사위 세개/주사위 세개.c
+++ 백준/Bronze/2480. 주사위 세개/주사위 세개.c
@@ -1,32 +1,25 @@
-#include<stdio.h>
-int main(){
-    int a,b,c,m;
-    scanf("%d %d %d",&a,&b,&c);
-    if(a==b){
-        if(b==c){
-            m=10000+(a*1000);
-        }else{
-            m=1000+(a*100);
-        } 
-    }else{
-        if(b==c){
-            m=1000+(b*100);
-        }else if(a==c){
-            m=1000+(a*100);
-        }else if(a>b){
-            if(a>c){
-                m=a*100;
-            }else{
-                m=c*100;
-            }
-        }else{
-            if(b>c){
-                m=b*100;
-            }else{
-                m=c*100;
-            }
-        }   
+#include <stdio.h>
+
+int main() {
+    int a, b, c;
+    int prize;
+
+    scanf("%d %d %d", &a, &b, &c);
+
+    if (a == b && b == c) {
+        prize = 10000 + a * 1000;
+    } else if (a == b || a == c) {
+        prize = 1000 + a * 100;
+    } else if (b == c) {
+        prize = 1000 + b * 100;
+    } else {
+        int max = a;
+        if (b > max) max = b;
+        if (c > max) max = c;
+        prize = max * 100;
     }
-    printf("%d",m);
+
+    printf("%d\n", prize);
+
     return 0;
 }
