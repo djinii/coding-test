25a634bb4f57584cbc8d8f4ca40f1e65a76b597a
djinii <129668708+djinii@users.noreply.github.com>
2025-03-14 20:46:20 +0900

    [Bronze V] Title: 개수 세기, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/10807. 개수 세기/개수 세기.py
+++ 백준/Bronze/10807. 개수 세기/개수 세기.py
@@ -1,15 +1,5 @@
 n = int(input())
+lst = list(map(int, input().split()))
+v = int(input())
 
-lst = list(map(int,input().split()))
-
-sn = int(input())
-
-cnt = 0
-
-for i in range(n):
-
-    if lst[i] == sn:
-
-        cnt += 1
-
-print(cnt)
+print(lst.count(v))
