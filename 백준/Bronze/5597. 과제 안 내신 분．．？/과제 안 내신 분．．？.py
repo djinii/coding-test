25b8a0eb44f633936eefa236549ed5782abcb489
djinii <129668708+djinii@users.noreply.github.com>
2025-03-20 17:45:57 +0900

    [Bronze III] Title: 과제 안 내신 분..?, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/5597. 과제 안 내신 분．．？/과제 안 내신 분．．？.py
+++ 백준/Bronze/5597. 과제 안 내신 분．．？/과제 안 내신 분．．？.py
@@ -1,13 +1,7 @@
 students = [0] * 30
-[0, 1, 0, 1, 1, ]
-
 for i in range(28) :
-    n = int(input())
-    students[n-1] = 1
-
-a = students.index(0)
-print(a+1)
-students[a] = 1
-
-a = students.index(0)
-print(a+1)
+    num = int(input())
+    students[num-1] = 1
+for j in range(30) :
+    if students[j] == 0 :
+        print(j+1)
