6c27b443598792365a7b910f62ae5b35b8fcd2a3
djinii <129668708+djinii@users.noreply.github.com>
2025-07-23 22:44:23 +0900

    [level 1] Title: 가장 많이 받은 선물, Time: 6.24 ms, Memory: 9.94 MB -BaekjoonHub

--- 프로그래머스/1/258712. 가장 많이 받은 선물/가장 많이 받은 선물.py
+++ 프로그래머스/1/258712. 가장 많이 받은 선물/가장 많이 받은 선물.py
@@ -1,33 +1,3 @@
 def solution(friends, gifts):
     answer = 0
-
-    gift_level = {name:0 for name in friends}
-    gift_count = {friend:{name:0 for name in friends}for friend in friends}
-
-    for gift in gifts :
-        sender, receiver = gift.split()
-        gift_count[sender][receiver] += 1
-        gift_level[sender] += 1
-        gift_level[receiver] -= 1
-
-    next_gift = {friend:0 for friend in friends}
-
-    for i in range(len(friends)) :
-        for j in range(len(friends)) :
-            if i == j :
-                continue
-            x = friends[i]
-            y = friends[j]
-
-            xToy_cnt = gift_count[x][y]
-            yTox_cnt = gift_count[y][x]
-
-            if xToy_cnt > yTox_cnt :
-                next_gift[x] += 1
-            elif xToy_cnt == yTox_cnt :
-                if gift_level[x] > gift_level[y] :
-                    next_gift[x] += 1
-
-    answer = max(next_gift.values())
-        
     return answer
